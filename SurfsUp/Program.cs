30c353716a477c72192330365265465e69788b7e
gubbih <tlha37727@edu.ucl.dk>
2022-09-23 11:57:15 +0200

    Eorrorroro 400 no clue

--- SurfsUp/Program.cs
+++ SurfsUp/Program.cs
@@ -17,7 +17,18 @@ builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.Requ
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+var config = builder.Configuration;
 
+
+builder.Services.AddAuthentication()
+
+    .AddGoogle(options =>
+    {
+        IConfigurationSection googleAuthNSection =
+        config.GetSection("Authentication:Google");
+        options.ClientId = googleAuthNSection["client_id"];
+        options.ClientSecret = googleAuthNSection["client_secret"];
+    });
 
 var app = builder.Build();
 using (var scope = app.Services.CreateScope())
@@ -38,14 +49,7 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-var configuration = builder.Configuration;
-var service = builder.Services;
-service.AddAuthentication().AddGoogle(googleOptions =>
-{
-    googleOptions.ClientId = configuration["Authentication:Google:692943574317-3g6koeqrghmhn8m10hdo09t1ifqmtjnn.apps.googleusercontent.com"];
-    googleOptions.ClientSecret = configuration["Authentication:Google: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"];
-    
-});
+
 
 var defaultCulture = new CultureInfo("de-DE");
 var localizationOptions = new RequestLocalizationOptions
