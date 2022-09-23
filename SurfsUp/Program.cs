427cdec9ee2a09bf612c6ae86259f29efbe8de9a
gubbih <tlha37727@edu.ucl.dk>
2022-09-23 10:09:57 +0200

    Added google to backend

--- SurfsUp/Program.cs
+++ SurfsUp/Program.cs
@@ -20,7 +20,6 @@ builder.Services.AddControllersWithViews();
 
 
 var app = builder.Build();
-
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
@@ -39,6 +38,14 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+var configuration = builder.Configuration;
+var service = builder.Services;
+service.AddAuthentication().AddGoogle(googleOptions =>
+{
+    googleOptions.ClientId = configuration["Authentication:Google:692943574317-3g6koeqrghmhn8m10hdo09t1ifqmtjnn.apps.googleusercontent.com"];
+    googleOptions.ClientSecret = configuration["Authentication:Google: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"];
+    
+});
 
 var defaultCulture = new CultureInfo("de-DE");
 var localizationOptions = new RequestLocalizationOptions
