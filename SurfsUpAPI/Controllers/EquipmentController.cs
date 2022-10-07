f2fee0612ece95444fe44277b266322ab8b6bef9
Martin Johannes Rønnekjær Jensen <martin.jensen.1997@hotmail.com>
2022-10-07 14:34:45 +0200

    Andet?

---
+++ SurfsUpAPI/Controllers/EquipmentController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SurfsUp.Data;
+using SurfsUp.Models;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SurfsUpAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EquipmentController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EquipmentController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public List<Equipment> Get()
+        {
+            IQueryable<Equipment> Equipment = from s in _context.Equipment select s;
+            List<Equipment> equipment = Equipment.ToList();
+            return equipment;
+        }
+    }
+}
