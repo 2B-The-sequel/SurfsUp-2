64c99498971b3f7d53f209ee18436dc3ef0c269d
peterskov1997 <peterskov1997@live.dk>
2022-10-09 23:52:43 +0200

    CRUD

--- SurfsUpAPI/Controllers/EquipmentController.cs
+++ SurfsUpAPI/Controllers/EquipmentController.cs
@@ -3,6 +3,13 @@ using SurfsUp.Data;
 using SurfsUp.Models;
 using System.Linq;
 using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using SurfsUp.Data.Migrations;
+using Equipment = SurfsUp.Models.Equipment;
 
 namespace SurfsUpAPI.Controllers
 {
@@ -18,11 +25,113 @@ namespace SurfsUpAPI.Controllers
         }
 
         [HttpGet]
-        public List<Equipment> Get()
+        public async Task<ActionResult> GetEquipments()
         {
-            IQueryable<Equipment> Equipment = from s in _context.Equipment select s;
-            List<Equipment> equipment = Equipment.ToList();
-            return equipment;
+            try
+            {
+                List<Equipment> equipment = await _context.Equipment.ToListAsync();
+                return Ok(equipment);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error retrieving data from the database");
+            }
         }
+
+        //Finder det Equipment objekt med det tilhørende id fra db
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Equipment>> GetEquipment(int id)
+        {
+            try
+            {
+                if (id == null || _context.Equipment == null)
+                {
+                    return NotFound();
+                }
+
+                Equipment equipment = await _context.Equipment.FirstOrDefaultAsync(m => m.EquipmentId == id);
+                if (equipment == null)
+                {
+                    return NotFound();
+                }
+                return Ok(equipment);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, 
+                                  "Error retrieving data from the database");
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Equipment>> CreateEquipment(Equipment equipment)
+        {
+            try
+            {
+                if(equipment == null)
+                { return BadRequest(); }
+                _context.Add(equipment);
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                                  "Error retrieving data from the database");
+            }
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult<Equipment>> UpdateEquipment(int id, Equipment equipment)
+        {
+            try
+            {
+                if (id != equipment.EquipmentId)
+                {
+                    return BadRequest("Equipment ID mismatch");
+                }
+                var equipmentToUpdate = await _context.Equipment.FindAsync(id);
+                if (equipmentToUpdate == null)
+                { 
+                    return NotFound($"Equipment with id = {id} not found "); 
+                 }
+
+                equipmentToUpdate.Name = equipment.Name;
+                equipmentToUpdate.Boards = equipment.Boards;
+                equipmentToUpdate.BoardEquipments = equipment.BoardEquipments;
+
+                _context.Update(equipmentToUpdate);
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                                  "Error retrieving data from the database");
+            }
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> DeleteEquipment (int id)
+        {
+            try
+            {
+                var equipmentToDelete = await _context.Equipment.FindAsync(id);
+                if(equipmentToDelete == null)
+                {
+                    return NotFound($"Equipment with id:{id} could not be found");
+                }
+                _context.Remove(equipmentToDelete);
+               await _context.SaveChangesAsync();
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                                  "Error retrieving data from the database");
+            }
+
+        }
+
     }
 }
