5553fe43449a7691664628640c8d9ed1364dd492
peterskov1997 <peterskov1997@live.dk>
2022-12-09 10:31:08 +0100

    ddddd

---
+++ SurfsUpBlazor/Client/Pages/DisplayWeatherBase.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace SurfsUpBlazor.Client.Pages
+{
+    public class DisplayWeatherBase : ComponentBase
+    {
+        [Parameter]
+        public int DaysShown { get; set; } = 1;
+
+
+
+
+    }
+}
